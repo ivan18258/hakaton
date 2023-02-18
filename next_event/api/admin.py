da91105af9728bebdc9516315837eb9b80672131
ivanb <ivanbor2016@gmail.com>
2023-02-18 12:43:56 +0300

    Админка, urls

---
+++ next_event/api/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from events import Event
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    """Событие"""
+    fields = (
+        'title',
+        'description',
+        'time',
+        'reminder_to_event_1',
+        'reminder_to_event_1',
+        'follower',
+    )
+
+    list_display = (
+        'title',
+        'description',
+        'time',
+        'reminder_to_event_1',
+        'reminder_to_event_1',
+        'follower',
+    )
